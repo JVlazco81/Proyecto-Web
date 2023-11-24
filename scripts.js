e2f955ab72d145f8bc8794b1ffd3193da40dd5ee
JVlazco81 <al062648@uacam.mx>
2023-11-23 23:15:01 -0600

    :tada: termine el proyecto

---
+++ scripts.js
@@ -0,0 +1,99 @@
+let botonRandom
+let botonRaza
+let imagen
+let selectorRaza
+let etiquetaContadora
+let contador = 0
+let tabla
+let botonLike
+let imagenID = 0
+
+function init(){
+	botonRandom = document.getElementById('botonPerroAleatorio')
+	botonRaza = document.getElementById('botonPerroRaza')
+	imagen = document.getElementById('imgRandom')
+	selectorRaza = document.getElementById('selectorRaza')
+	etiquetaContadora = document.getElementById('contador')
+	tabla = document.querySelector('#tabla > tbody')
+	botonLike = document.getElementById('like')
+
+	botonRandom.addEventListener('click',getDataRandom)
+	botonRaza.addEventListener('click',getDataRaza)
+	botonLike.addEventListener('click',darLike)
+}
+
+async function getDataRandom(){
+	try{
+		const promise = await fetch('https://dog.ceo/api/breeds/image/random')
+		const perro = await promise.json()
+		imagen.src = perro.message
+		contarVistas()
+
+	}catch(err){
+		alert('Ocurrió un error ' + err)
+	}
+	playAudio('pulsar')
+	imagenID++
+	gestorLikes = false
+}
+
+async function getDataRaza(){
+	try{
+		const promise = await fetch(`https://dog.ceo/api/breed/${selectorRaza.value}/images/random`)
+		const perro = await promise.json()
+		imagen.src = perro.message
+		contarVistas()
+	}catch(err){
+		alert('Ocurrió un error ' + err)
+	}
+	playAudio('pulsar')
+	imagenID++
+	gestorLikes = false
+}
+
+function playAudio(opcion){
+	let audio = document.createElement('audio')
+	
+	if(opcion === 'like'){
+		audio.setAttribute('src','recursos/like.mpeg')
+		audio.play()
+	}else if(opcion === 'pulsar'){
+		audio.setAttribute('src','recursos/pulsar.mpeg')
+		audio.play()
+	}else if(opcion === 'eliminar'){
+		audio.setAttribute('src','recursos/eliminar.mpeg')
+		audio.play()
+	}
+}
+
+function contarVistas(){
+	contador++
+	etiquetaContadora.innerText = contador
+}
+
+function darLike(){
+	var html = ''
+	html = `
+	<tr>
+		<td>${imagen.src}</td>
+		<td>
+			<img src="${imagen.src}" class="mediano" data-id="${imagenID}">
+		</td>
+		<td>
+			<button class="btn-eliminar btn btn-danger" title="Eliminar de los likes" data-id="${imagenID}">X</button>
+		</td>
+	</tr>
+	`
+	tabla.innerHTML += html
+
+	document.querySelectorAll(".btn-eliminar").forEach(boton => {
+		boton.addEventListener("click",() => {
+			playAudio('eliminar')
+			fila = document.querySelector(`button[data-id='${boton.dataset.id}']`)
+			fila.closest('tr').remove()
+		})
+	})
+	playAudio('like')
+	imagenID++
+}
+window.addEventListener('load', init)
